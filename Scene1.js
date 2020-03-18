d8b317a5c3e744f8c0e36e00d54f2cfbe0200152
HacquinA <athenais.hacquin@gmail.com>
2020-03-18 18:25:32 +0100

    ajout sp3 et deplacement

--- Scene1.js
+++ Scene1.js
@@ -3,19 +3,26 @@ class Scene1 extends Phaser.Scene{
 		super("Scene_1");
 	}
 
+
 	init(data){
     	var platforms;
     	var sol;
     	var cursors;
     	var player;
+   		var bullet;
+    	
 	}
+
+
 
 	preload(){
 		this.load.image('espace','assets/espace.png');
-		this.load.spritesheet('sp1','assets/SpaceInvader1.png',{frameWidth: 20, frameHeight: 8});
-		this.load.spritesheet('vaisseau','assets/vaisseau.png',{frameWidth: 27.3, frameHeight: 20});
+		this.load.spritesheet('sp3','assets/SpaceInvader3.png',{frameWidth: 18.6, frameHeight: 16});
+		this.load.spritesheet('vaisseau','assets/vaisseau.png',{frameWidth: 27.6, frameHeight: 20});
 		this.load.image('platform','assets/platform.png');
+		this.load.image('bullet','assets/bullet.png');
 	}
+
 
 	create(){
 		this.add.image(512,385,'espace');
@@ -39,12 +46,12 @@ class Scene1 extends Phaser.Scene{
 		this.player.body.setGravityY(200);
 		this.physics.add.collider(this.player,this.platforms);
 
-	//Récupération des curseurs
-		/*this.keys = this.input.keyboard.addKeys('A,S,P');
+	/*//Récupération des curseurs
+		this.keys = this.input.keyboard.addKeys('A,S,P');
 		this.cursors = this.input.keyboard.createCursorKeys();
-		this.fire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);*/
-
-	// deplacement perso
+		this.fire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+*/
+	// anims perso
 
 		this.anims.create({
 		    key: 'left',
@@ -65,6 +72,64 @@ class Scene1 extends Phaser.Scene{
 		    frameRate: 10,
 		    repeat: -1
 		});
+
+	// Tir perso 
+		this.bullet = this.physics.add.staticGroup();
+		
+
+		this.keyObj = this.input.keyboard.addKey('W');  // Get key object
+			
+			this.keyObj.on('down', function(event) { 
+				this.bullet.create(512,760,'bullet');
+				this.bullet = this.physics.add.sprite(500,450,'bullet');
+				this.bullet.setVelocityX(200);
+				this.physics.add.collider(this.player,this.platforms, this.sp1);
+			});
+			
+			this.keyObj.on('up', function(event) { 
+				this.bullet = this.physics.add.sprite(500,450,'bullet');
+				this.bullet.setVelocityX(200);
+				this.physics.add.collider(this.player,this.platforms, this.sp1);
+			});
+
+	//sp3
+
+		this.sp3 = this.physics.add.sprite(300,100,'sp3');
+		this.sp3.setCollideWorldBounds(true);
+
+
+		this.sp3s = this.physics.add.group();
+		this.physics.add.overlap(this.player,this.playerj);
+		this.physics.add.collider(this.player, this.sp3, null, this);
+		this.physics.add.collider(this.sp3,this.sol);
+		this.sp3.body.setGravityY(-300);
+
+		
+		
+
+		
+		this.anims.create({
+			key: 'sp3',
+			frames: this.anims.generateFrameNumbers('sp3', {start: 0, end: 2}),
+			frameRate: 5,
+			repeat: -1
+		});
+
+		this.anims.create({
+			key: 'mvt_sp3',
+			frames: this.anims.generateFrameNumbers('sp3', {start: 0, end: 2}),
+			frameRate: 5,
+			repeat: -1
+		});
+
+
+
+
+
+
+
+
+
 }
 
 	update(){
@@ -80,32 +145,92 @@ class Scene1 extends Phaser.Scene{
 
 		if (this.cursors.left.isDown)
 		{
-		    this.player.setVelocityX(-160);
+		   this.player.setVelocityX(-200);
 
-		    //this.player.anims.play('left', true);
+		   this.player.anims.play('left', true);
+
+		   this.player.direction = 'left';
 		}
 		else if (this.cursors.right.isDown)
 		{
-		    this.player.setVelocityX(160);
+		    this.player.setVelocityX(200);
 
-		    //this.player.anims.play('right', true);
+		    this.player.anims.play('right', true);
+
+		    this.player.direction = 'right';
 		}
 		else
 		{
 		    this.player.setVelocityX(0);
 
-		    //this.player.anims.play('turn');
+		    this.player.anims.play('turn');
 		}
 
-		if (this.cursors.up.isDown && player.body.touching.down)
+		if (this.cursors.up.isDown && this.player.body.touching.down){    
+
+			this.player.setVelocityY(-330);
+		}
+
+ 		
+	// Deplacement sp3
+	/*
+		if (this.cursors.left.isDown)
 		{
-		    this.player.setVelocityY(-330);
+		   this.player.setVelocityX(-200);
+
+		   this.player.anims.play('left', true);
+
+		   this.player.direction = 'left';
+		}
+		else if (this.cursors.right.isDown)
+		{
+		    this.player.setVelocityX(200);
+
+		    this.player.anims.play('right', true);
+
+		    this.player.direction = 'right';
+		}
+		else
+		{
+		    this.player.setVelocityX(0);
+
+		    this.player.anims.play('turn');
 		}
 
+		if (this.cursors.up.isDown && this.player.body.touching.down){    
+
+			this.player.setVelocityY(-330);
+		}
+*/
+	//mvt sp3 
 
 
+	if (this.sp3.x >= 300){
+    	this.tweens.add({
+	    	targets: this.sp3,
+	   	 	x : -100,
+	    	ease: 'Linear',       // 'Cubic', 'Elastic', 'Bounce', 'Back'
+	    	duration: 5000,
+	    	repeat: 0,            // -1: infinity
+	    	yoyo: false
+		});
+		this.sp3.anims.play('mvt_sp3', true);
+		this.sp3.setFlipY(false);
+	}
+	
+	if (this.sp3.x <= 100){
+		this.tweens.add({
+	    	targets: this.sp3,
+			x : 520,
+	    	ease: 'Linear',       // 'Cubic', 'Elastic', 'Bounce', 'Back'
+	    	duration: 5000,
+	    	repeat: 0,            // -1: infinity
+	    	yoyo: false
+		});
+		this.sp3.anims.play('mvt_sp3', true);
+		this.sp3.setFlipY(false);
 
-
+	}
 	}
 
 }
